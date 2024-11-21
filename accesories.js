8ad4c01431ad7d24c69e3411eb3c39fbc7b3de13
adriana-444 <aaa444@njit.edu>
2024-11-21 16:33:58 -0500

    Adriana added content to Java Script on Accesories page

---
+++ accesories.js
@@ -0,0 +1,10 @@
+function generateStars(rating) {
+    console.log('Generating stars for rating:',rating);
+    let stars = '';
+    for (let i= <= 5; i++) {
+        stars += i <= rating? 
+    }
+    console.log(' Generated stars:', stars);
+    return stars; 
+}
+
